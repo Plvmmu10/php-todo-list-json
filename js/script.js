92d7ca77dcd0ff424c8ec3598a7b48d73448e720
peppeluc22 <gluca0260@gmail.com>
2023-05-08 14:12:44 +0200

    add CreateList method

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            title: 'Todo List'
+            title: 'Todo List',
+            urlAPI: './server.php',
+            todoList: []
         }
     },
     methods: {
-
+        createList() {
+            axios.get(this.urlAPI).then((res) => {
+                this.todoList = res.data;
+                console.log(todoList);
+            })
+        }
     },
     mounted() {
-
+        this.createList();
     }
 }).mount('#app');
